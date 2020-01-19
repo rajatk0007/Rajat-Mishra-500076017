f8f9ad1ba3c219e570ff85ec861583f84d9df9ca
rajatk0007 <60063926+rajatk0007@users.noreply.github.com>
2020-01-19 17:27:42 +0530

    Add files via upload

---
+++ question 2.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 18 23:04:28 2020
+
+@author: Dell
+"""
+
+"""Amonng the propositions supplied we only select those propositions which correspond to our conclusion"""
+from sympy import*
+from sympy.abc import P,Q,R,S,T,U,V,W
+P='Every Human'
+Q='Good Graders'
+R='Study Well'
+l=[]
+P=[True,True,True,True,False,False,False,False]
+Q=[True,True,False,False,True,True,False,False]
+R=[True,False,True,False,True,False,True,False]
+S=[]
+T=[]
+U=[]
+V=[]
+W=[]
+for i in range(8):
+    S.append(Implies(Q[i],R[i]))#Good graders study well
+
+for i in range(8):
+    T.append(P[i] and not(R[(i)]))#Every human does not study well
+
+for i in range(8):
+    U.append((S[i] and T[i]))
+
+for i in range(8):
+    V.append(P[i] and Q[i])#Conclusion:Every human is a good grader
+
+for i in range(8):
+    W.append(Implies(U[i],V[i]))
+
+
+for i in W:
+    if(i==False):
+        print('No')
+    else:
+        continue
+
+
